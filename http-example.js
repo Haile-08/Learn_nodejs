704bce3cc468b939da330f65eaba90c6d148ab9c
Haile-08 <haile4cmd@gmail.com>
2022-07-07 10:04:52 +0300

    http

--- http-example.js
+++ http-example.js
@@ -1,6 +1,34 @@
-const http = require("http");
+// const http = require("http");
 
-const req = http.request("http://www.google.com", (res) => {
+// const req = http.request("http://www.google.com", (res) => {
+//   res.on("data", (chunk) => {
+//     console.log(`Data chunk: ${chunk}`);
+//   });
+//   res.on("end", () => {
+//     console.log("No more data");
+//   });
+// });
+
+// req.end();
+
+// how to use request function without calling it
+// const { request } = require("https");
+
+// const req = request("http://www.google.com", (res) => {
+//   res.on("data", (chunk) => {
+//     console.log(`Data chunk: ${chunk}`);
+//   });
+//   res.on("end", () => {
+//     console.log("No more data");
+//   });
+// });
+
+// req.end();
+
+// using get to get data does the same thing as request
+const { get } = require("https");
+
+const req = get("http://www.google.com", (res) => {
   res.on("data", (chunk) => {
     console.log(`Data chunk: ${chunk}`);
   });
