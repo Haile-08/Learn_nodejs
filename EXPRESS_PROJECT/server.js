30d2a704cdd68cce1d9a8e9323f78f3549bd16ac
Haile-08 <haile4cmd@gmail.com>
2022-07-09 14:03:07 +0300

    posting data using express

--- EXPRESS_PROJECT/server.js
+++ EXPRESS_PROJECT/server.js
@@ -14,17 +14,34 @@ const friends = [
     name: "haile",
   },
 ];
-
+//********************************/
 // Adding middleware to the server
 app.use((req,res,next) =>{
    console.log(`${req.method} ${req.url}`);
    next();
 })
+//********************************/
 
-
+//********** other midlewar 2*****/
+app.use(expresss.json())
+/// post
+app.post("/friends", (req, res) => {
+if(!req.body.name){
+    return res.status(400).json({
+        error: 'missing friend name',
+    })
+}
+  const newFriend = {
+    name: req.body.name,
+    id: friends.length
+  };
+  friends.push(newFriend);
+  res.json(friends)
+});
 app.get("/friends", (req, res) => {
   res.json(friends);
 });
+
 app.get("friends/:friendID", (req, res) => {
   const friendID = Number(req.params.friendID);
   const friend = friends[friendID];
