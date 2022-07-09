dd072852f5b79a72e24bc926b4c83337221490e7
Haile-08 <haile4cmd@gmail.com>
2022-07-09 13:33:24 +0300

    adding middleware to express

--- EXPRESS_PROJECT/server.js
+++ EXPRESS_PROJECT/server.js
@@ -15,6 +15,13 @@ const friends = [
   },
 ];
 
+// Adding middleware to the server
+app.use((req,res,next) =>{
+   console.log(`${req.method} ${req.url}`);
+   next();
+})
+
+
 app.get("/friends", (req, res) => {
   res.json(friends);
 });
