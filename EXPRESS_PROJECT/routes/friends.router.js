2c91410c477ffeb8ce9f6ef512ff45a25a2bcd2c
Haile-08 <haile4cmd@gmail.com>
2022-07-10 10:34:45 +0300

    adding a middleware to friendsrouter

--- EXPRESS_PROJECT/routes/friends.router.js
+++ EXPRESS_PROJECT/routes/friends.router.js
@@ -4,6 +4,10 @@ const friendsController = require("../controllers/friends.controllers");
 
 const friendsrouter = express.Router();
 
+friendsrouter.use((req, res, next) => {
+  console.log("ip address: ", req.ip);
+  next();
+});
 friendsrouter.post("/", friendsController.postFriend);
 friendsrouter.get("/", friendsController.getFriends);
 friendsrouter.get("/:friendID"), friendsController.getFriend;
