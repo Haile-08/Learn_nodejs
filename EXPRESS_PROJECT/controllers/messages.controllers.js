adc8927bc1593be2ef9e8d0d643e693596b75b08
Haile-08 <haile4cmd@gmail.com>
2022-07-10 11:56:24 +0300

    sending image files in nodejs

--- EXPRESS_PROJECT/controllers/messages.controllers.js
+++ EXPRESS_PROJECT/controllers/messages.controllers.js
@@ -1,5 +1,9 @@
+//to pass the path on any device
+const path = require("path");
+
 function getMessages(req, res) {
-  res.send("<h1>hello worldx</h1>");
+  res.sendFile(path.join(__dirname, "..", "public", "image.png"));
+  //res.send("<h1>hello worldx</h1>");
 }
 // best to use a named function for identifying errors
 function postMessage(req, res) {
